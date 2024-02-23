4434cf6e97981a41c610c7bb842e9dd522fa9ec1
MakiWolf <73300633+MakiWolf@users.noreply.github.com>
2024-02-23 10:38:42 +0100

    update welcome page

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.WelcomePage/WelcomePageListButton.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.WelcomePage/WelcomePageListButton.cs
@@ -29,7 +29,7 @@ using System.Collections.Generic;
 using MonoDevelop.Core;
 using MonoDevelop.Components;
 using MonoDevelop.Components.AtkCocoaHelper;
-
+using Cairo;
 using Gtk;
 
 namespace MonoDevelop.Ide.WelcomePage
@@ -331,61 +331,61 @@ namespace MonoDevelop.Ide.WelcomePage
 			}
 		}
 
-//		protected override bool OnExposeEvent (Gdk.EventExpose evnt)
-//		{
-//			using (var ctx = Gdk.CairoHelper.Create (evnt.Window)) {
-//				if (mouseOver)
-//					DrawHoverBackground (ctx);
+		protected override bool OnDrawn (Cairo.Context evnt)
+		{
+			using (var ctx = Gdk.CairoHelper.Create (GdkWindow)) {
+				if (mouseOver)
+					DrawHoverBackground (ctx);
 //
 //				// Draw the icon
-//				DrawIcon (ctx);
+				DrawIcon (ctx);
 //
 //				// Draw the text
 //
-//				int textWidth = Allocation.Width - LeftTextPadding - InternalPadding * 2;
+				int textWidth = Allocation.Width - LeftTextPadding - InternalPadding * 2;
 //
-//				using (var titleLayout = new Pango.Layout (PangoContext))
-//				{
-//					titleLayout.Width = Pango.Units.FromPixels (textWidth);
-//					titleLayout.Ellipsize = Pango.EllipsizeMode.End;
-//					titleLayout.SetMarkup (WelcomePageSection.FormatText (TitleFontFace, TitleFontSize, Pango.Weight.Bold, MediumTitleColor, title));
+				using (var titleLayout = new Pango.Layout (PangoContext))
+				{
+					titleLayout.Width = Pango.Units.FromPixels (textWidth);
+					titleLayout.Ellipsize = Pango.EllipsizeMode.End;
+					titleLayout.SetMarkup (WelcomePageSection.FormatText (TitleFontFace, TitleFontSize, Pango.Weight.Bold, MediumTitleColor, title));
 //
-//					Pango.Layout subtitleLayout = null;
+					Pango.Layout subtitleLayout = null;
 //
-//					if (!string.IsNullOrEmpty (subtitle))
-//					{
-//						subtitleLayout = new Pango.Layout (PangoContext);
-//						subtitleLayout.Width = Pango.Units.FromPixels (textWidth);
-//						subtitleLayout.Ellipsize = Pango.EllipsizeMode.Start;
-//						subtitleLayout.SetMarkup (WelcomePageSection.FormatText (SmallTitleFontFace, SmallTitleFontSize, Pango.Weight.Normal, SmallTitleColor, subtitle));
-//					}
+					if (!string.IsNullOrEmpty (subtitle))
+					{
+						subtitleLayout = new Pango.Layout (PangoContext);
+						subtitleLayout.Width = Pango.Units.FromPixels (textWidth);
+						subtitleLayout.Ellipsize = Pango.EllipsizeMode.Start;
+						subtitleLayout.SetMarkup (WelcomePageSection.FormatText (SmallTitleFontFace, SmallTitleFontSize, Pango.Weight.Normal, SmallTitleColor, subtitle));
+					}
 //
-//					int height = 0;
-//					int w, h1, h2;
-//					titleLayout.GetPixelSize (out w, out h1);
-//					height += h1;
+					int height = 0;
+					int w, h1, h2;
+					titleLayout.GetPixelSize (out w, out h1);
+					height += h1;
 //
-//					if (subtitleLayout != null)
-//					{
-//						height += Styles.WelcomeScreen.Pad.Solutions.SolutionTile.TitleBottomMargin;
-//						subtitleLayout.GetPixelSize (out w, out h2);
-//						height += h2;
-//					}
+					if (subtitleLayout != null)
+					{
+						height += Styles.WelcomeScreen.Pad.Solutions.SolutionTile.TitleBottomMargin;
+						subtitleLayout.GetPixelSize (out w, out h2);
+						height += h2;
+					}
 //
-//					int tx = Allocation.X + InternalPadding + LeftTextPadding;
-//					int ty = Allocation.Y + (Allocation.Height - height) / 2;
-//					DrawLayout (ctx, titleLayout, TitleFontFace, TitleFontSize, Pango.Weight.Bold, MediumTitleColor, tx, ty);
+					int tx = Allocation.X + InternalPadding + LeftTextPadding;
+					int ty = Allocation.Y + (Allocation.Height - height) / 2;
+					DrawLayout (ctx, titleLayout, TitleFontFace, TitleFontSize, Pango.Weight.Bold, MediumTitleColor, tx, ty);
 //
-//					if (subtitleLayout != null)
-//					{
-//						ty += h1 + Styles.WelcomeScreen.Pad.Solutions.SolutionTile.TitleBottomMargin;
-//						DrawLayout (ctx, subtitleLayout, SmallTitleFontFace, SmallTitleFontSize, Pango.Weight.Normal, SmallTitleColor, tx, ty);
-//						subtitleLayout.Dispose ();
-//					}
-//				}
-//			}
-//			return true;
-//		}
+					if (subtitleLayout != null)
+					{
+						ty += h1 + Styles.WelcomeScreen.Pad.Solutions.SolutionTile.TitleBottomMargin;
+						DrawLayout (ctx, subtitleLayout, SmallTitleFontFace, SmallTitleFontSize, Pango.Weight.Normal, SmallTitleColor, tx, ty);
+						subtitleLayout.Dispose ();
+					}
+				}
+			}
+			return true;
+		}
 
 		protected override void OnDestroyed ()
 		{
