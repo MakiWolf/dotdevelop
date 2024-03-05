f6c72b11bd59b418ed4f34be6c8f2bc696a389e7
MakiWolf <73300633+MakiWolf@users.noreply.github.com>
2024-03-05 12:03:01 +0100

    update

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/Gui/MonoDevelop.VersionControl.Git.PushDialog.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/Gui/MonoDevelop.VersionControl.Git.PushDialog.cs
@@ -7,9 +7,9 @@ namespace MonoDevelop.VersionControl.Git
 		private global::Gtk.VBox vbox2;
 		private global::Gtk.HBox hbox1;
 		private global::Gtk.Label label1;
-		private global::Gtk.ComboBox remoteCombo;
+		private global::Gtk.ComboBoxText remoteCombo;
 		private global::Gtk.Label label2;
-		private global::Gtk.ComboBox branchCombo;
+		private global::Gtk.ComboBoxText branchCombo;
 		private global::MonoDevelop.VersionControl.Views.ChangeSetView changeList;
 		private global::Gtk.Button buttonCancel;
 		private global::Gtk.Button buttonOk;
@@ -22,9 +22,9 @@ namespace MonoDevelop.VersionControl.Git
 			this.Title = global::Mono.Unix.Catalog.GetString ("Push to Repository");
 			this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 			// Internal child MonoDevelop.VersionControl.Git.PushDialog.VBox
-			// global::Gtk.VBox w1 = this.VBox;
-			// w1.Name = "dialog1_VBox";
-			// w1.BorderWidth = ((uint)(2));
+			global::Gtk.VBox w1 = (Gtk.VBox)this.ContentArea;
+			w1.Name = "dialog1_VBox";
+			w1.BorderWidth = ((uint)(2));
 			// Container child dialog1_VBox.Gtk.Box+BoxChild
 			this.vbox2 = new global::Gtk.VBox ();
 			this.vbox2.Name = "vbox2";
@@ -44,11 +44,11 @@ namespace MonoDevelop.VersionControl.Git
 			w2.Expand = false;
 			w2.Fill = false;
 			// Container child hbox1.Gtk.Box+BoxChild
-			// this.remoteCombo = global::Gtk.ComboBox.NewText ();
-			// this.remoteCombo.Name = "remoteCombo";
-			// this.hbox1.Add (this.remoteCombo);
-			// global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.remoteCombo]));
-			// w3.Position = 1;
+			this.remoteCombo = new global::Gtk.ComboBoxText ();
+			this.remoteCombo.Name = "remoteCombo";
+			this.hbox1.Add (this.remoteCombo);
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.remoteCombo]));
+			w3.Position = 1;
 			// Container child hbox1.Gtk.Box+BoxChild
 			this.label2 = new global::Gtk.Label ();
 			this.label2.Name = "label2";
@@ -59,9 +59,9 @@ namespace MonoDevelop.VersionControl.Git
 			w4.Expand = false;
 			w4.Fill = false;
 			// Container child hbox1.Gtk.Box+BoxChild
-			// this.branchCombo = global::Gtk.ComboBox.NewText ();
-			// this.branchCombo.Name = "branchCombo";
-			// this.hbox1.Add (this.branchCombo);
+			this.branchCombo = new global::Gtk.ComboBoxText ();
+			this.branchCombo.Name = "branchCombo";
+			this.hbox1.Add (this.branchCombo);
 			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.branchCombo]));
 			w5.Position = 3;
 			this.vbox2.Add (this.hbox1);
@@ -77,15 +77,15 @@ namespace MonoDevelop.VersionControl.Git
 			this.vbox2.Add (this.changeList);
 			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.changeList]));
 			w7.Position = 1;
-			// w1.Add (this.vbox2);
-			// global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.vbox2]));
-			// w8.Position = 0;
+			w1.Add (this.vbox2);
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.vbox2]));
+			w8.Position = 0;
 			// Internal child MonoDevelop.VersionControl.Git.PushDialog.ActionArea
-			// global::Gtk.HButtonBox w9 = this.ActionArea;
-			// w9.Name = "dialog1_ActionArea";
-			// w9.Spacing = 10;
-			// w9.BorderWidth = ((uint)(5));
-			// w9.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w9 = (Gtk.HButtonBox)this.ActionArea;
+			w9.Name = "dialog1_ActionArea";
+			w9.Spacing = 10;
+			w9.BorderWidth = ((uint)(5));
+			w9.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button ();
 			this.buttonCancel.CanDefault = true;
@@ -95,9 +95,9 @@ namespace MonoDevelop.VersionControl.Git
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget (this.buttonCancel, -6);
-			// global::Gtk.ButtonBox.ButtonBoxChild w10 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonCancel]));
-			// w10.Expand = false;
-			// w10.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w10 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonCancel]));
+			w10.Expand = false;
+			w10.Fill = false;
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button ();
 			this.buttonOk.CanDefault = true;
@@ -106,10 +106,10 @@ namespace MonoDevelop.VersionControl.Git
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = global::Mono.Unix.Catalog.GetString ("Push Changes");
 			this.AddActionWidget (this.buttonOk, -5);
-			// global::Gtk.ButtonBox.ButtonBoxChild w11 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonOk]));
-			// w11.Position = 1;
-			// w11.Expand = false;
-			// w11.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w11 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonOk]));
+			w11.Position = 1;
+			w11.Expand = false;
+			w11.Fill = false;
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
