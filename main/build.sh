93ea1748a0ff994387f7a8590280049c14f3b472
MakiWolf <73300633+MakiWolf@users.noreply.github.com>
2024-03-16 14:11:54 +0100

    update

--- main/build.sh
+++ main/build.sh
@@ -1,3 +1,5 @@
+nuget restore
+
 msbuild external/fsharpbinding/.paket/paket.targets /t:RestorePackages /p:PaketReferences="external\fsharpbinding\MonoDevelop.FSharpBinding\paket.references"
 
 msbuild Main.sln /bl:MonoDevelop.binlog /r /m /p:Configuration=DebugGnome /p:PLATFORM='Any CPU'
