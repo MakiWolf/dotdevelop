544b757fff4dec28f84d0c04ad261325e0d4b68c
makiwolf <makiwolf@outlook.at>
2025-02-02 00:48:10 +0100

    update

--- main/build.sh
+++ main/build.sh
@@ -1,5 +1,5 @@
-nuget restore
+dotnet restore
 
 msbuild external/fsharpbinding/.paket/paket.targets /t:RestorePackages /p:PaketReferences="external\fsharpbinding\MonoDevelop.FSharpBinding\paket.references"
 
-msbuild Main.sln /bl:MonoDevelop.binlog /r /m /p:Configuration=DebugGnome /p:PLATFORM='Any CPU'
+msbuild Main.sln /p:Configuration=DebugGnome /p:PLATFORM='Any CPU'
