d733991f49476e14ab34c3c78c14fda77563b6a3
makiwolf <makiwolf@outlook.at>
2025-02-02 02:53:16 +0100

    update

--- main/build.sh
+++ main/build.sh
@@ -1,5 +1,8 @@
 dotnet restore
 
+dotnet restore external/vs-editor-api
+dotnet restore external/Xamarin.PropertyEditing/Xamarin.PropertyEditing
+
 msbuild external/fsharpbinding/.paket/paket.targets /t:RestorePackages /p:PaketReferences="external\fsharpbinding\MonoDevelop.FSharpBinding\paket.references"
 
 msbuild Main.sln /p:Configuration=DebugGnome /p:PLATFORM='Any CPU'
